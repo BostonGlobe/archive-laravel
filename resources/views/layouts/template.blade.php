9ce7e8fd8415c61d9059773b0ed389d1cdd51f2c
Meixia-S <meixia.sindelar@globe.com>
2023-04-25 16:11:47 -0400

    Fixing banner ad margins

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -17,8 +17,8 @@
 </head>
 
 <body>
-    <div id="bannerAdSlot"> <img class="desktopAd" src="/ads/amazonAD.jpg" alt="Horizontal Ad" ></div>
-    <div id="bannerAdSlot_Mobile"> <img class="mobileAd" src="/ads/dellAD.png" alt="Mobile Horizontal Ad" ></div>
+    <div id="bannerAdSlot"> <img class="desktopAd bannerAdSlot" src="/ads/amazonAD.jpg" alt="Horizontal Ad" ></div>
+    <div id="bannerAdSlot_Mobile"> <img class="mobileAd bannerAdSlot_Mobile" src="/ads/dellAD.png" alt="Mobile Horizontal Ad" ></div>
     <div id="container" class="container">
         <div id="containerBorder">
             @include('layouts.header')
