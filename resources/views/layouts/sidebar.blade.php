cdb89fc6389d0a3f7fda471576b67c80da24a910
photocurio <sunraydust@gmail.com>
2023-04-25 14:08:58 -0400

    added sidebar include

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,14 @@
+<div id="Col2" class="col-lg-5">
+	<div id="Col2Top">
+	</div>
+	<div id="rightAd">
+		<div class="advertisement">Advertisement</div>
+		<div></div>
+	</div>
+	<div id="Col2LRCont">
+		<div id="Col2L"></div>
+		<div id="Col2R">
+			<div></div>
+		</div>
+	</div>
+</div>
