64d039c54e82bd88f879041b4e4805046fcd364e
photocurio <sunraydust@gmail.com>
2023-04-25 14:11:14 -0400

    added include to template

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -33,20 +33,7 @@
                         <div id="articleBottomAd"></div>
                     </div>
                 </div>
-                <div id="Col2" class="col-lg-5">
-                    <div id="Col2Top">
-                    </div>
-                    <div id="rightAd">
-                        <div class="advertisement">Advertisement</div>
-                        <div></div>
-                    </div>
-                    <div id="Col2LRCont">
-                        <div id="Col2L"></div>
-                        <div id="Col2R">
-                            <div></div>
-                        </div>
-                    </div>
-                </div>
+                @include('layouts.sidebar')
                 <div class="col-12">
                     <hr>
                 </div>
