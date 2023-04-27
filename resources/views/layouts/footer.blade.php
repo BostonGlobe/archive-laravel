d86ce31c1a107826d57766e9317256ef968fa19e
photocurio <sunraydust@gmail.com>
2023-04-27 12:03:07 -0400

    removed obsolete footer links, changed copyright

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,23 +1,18 @@
 <div id="footer">
 	<div id="bottomBanner">
-		<div class="bottomLinks" id="bottomLinks">
+		<div class="bottomLinks">
 			<ul id="bcomLinks">
-				<li class="first footerLinks"><a class="defaultEdit" href="http://www.boston.com/help/bostoncom_info/">Contact Boston.com</a></li>
-				<li class="footerLinks"><a class="defaultEdit" href="http://members.boston.com/reg/login.do?dispatch=loginpage&amp;p1=Foot_ContactBostonCom_Newsletters">Newsletters</a></li>
-				<li class="footerLinks"><a class="defaultEdit" href="http://www.boston.com/help/">Help</a></li>
-				<li class="footerLinks"><a class="defaultEdit" href="http://www.boston.com/help/privacy_policy/">Privacy Policy</a></li>
-			</ul>
-			<ul id="bglobeLinks">
-				<li class="first footerLinks"><a class="defaultEdit" href="http://bostonglobe.com/aboutus/contact_us/default.aspx">Contact The Boston Globe</a></li>
-				<li class="footerLinks"><a class="defaultEdit" href="http://services.bostonglobe.com/globestore/category.cgi?category=0&amp;source=boston.com&amp;kw=boston.com">The Boston Globe Store</a></li>
-				<li class="footerLinks"><a class="defaultEdit" href="http://bostonglobe.com/subscribers/extras/index.aspx">The Boston Globe Extras</a></li>
+				<li class="first footerLinks"><a class="defaultEdit" href="https://www.boston.com/contact-us/">Contact Boston.com</a></li>
+				<li class="footerLinks"><a class="defaultEdit" href="https://www.bostonglobe.com/newsletters/">Newsletters</a></li>
+				<li class="footerLinks"><a class="defaultEdit" href="https://customerservice.globe.com/hc/en-us/">Help</a></li>
+				<li class="footerLinks"><a class="defaultEdit" href="https://www.boston.com/privacy-policy/">Privacy Policy</a></li>
 			</ul>
 			<ul id="copyrightStamp">
 				<li>&copy;
 					<script>
 						var crYear = new Date();
 						document.write(crYear.getFullYear());
-					</script> NY Times Co.
+					</script> Boston Globe Media Partners
 				</li>
 			</ul>
 		</div>
