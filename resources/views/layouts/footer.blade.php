5f6932d98b2d07b62553a236c34947e94df7910d
photocurio <sunraydust@gmail.com>
2023-05-01 09:14:44 -0400

    fixed footer link, removed Franklin Gothic, styled storyend img

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,15 +3,14 @@
 		<div class="bottomLinks">
 			<ul id="bcomLinks">
 				<li class="first footerLinks"><a class="defaultEdit" href="https://www.boston.com/contact-us/">Contact Boston.com</a></li>
-				<li class="footerLinks"><a class="defaultEdit" href="https://www.bostonglobe.com/newsletters/">Newsletters</a></li>
+				<li class="footerLinks"><a class="defaultEdit" href="https://www.boston.com/tag/newsletters/">Newsletters</a></li>
 				<li class="footerLinks"><a class="defaultEdit" href="https://customerservice.globe.com/hc/en-us/">Help</a></li>
 				<li class="footerLinks"><a class="defaultEdit" href="https://www.boston.com/privacy-policy/">Privacy Policy</a></li>
 			</ul>
 			<ul id="copyrightStamp">
 				<li>&copy;
 					<script>
-						var crYear = new Date();
-						document.write(crYear.getFullYear());
+						document.write(new Date().getFullYear());
 					</script> Boston Globe Media Partners
 				</li>
 			</ul>
