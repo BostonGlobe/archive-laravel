524729fb159a864633fc43f989e94d835792f87b
photocurio <sunraydust@gmail.com>
2023-07-06 10:30:36 -0400

    changed search placeholder text

--- resources/views/partials/searchform.blade.php
+++ resources/views/partials/searchform.blade.php
@@ -1,3 +1,3 @@
 <form role="search" action="{{ route('search') }}" method="GET">
-	<input class="form-control bdc-archive-search" type="search" name="s" placeholder="Search" aria-label="Search">
+	<input class="form-control bdc-archive-search" type="search" name="s" placeholder="Search the archives" aria-label="Search">
 </form>
