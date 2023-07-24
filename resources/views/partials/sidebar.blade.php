404ff5e4e9dd30d3a3bb814da622f5b57e6d4290
photocurio <sunraydust@gmail.com>
2023-07-24 12:12:17 -0400

    Added more Wikipedia Commons images

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div id="rightAd">
 	<div class="advertisement">Advertisement</div>
-	<div id="verticalAdSlot"><img class="desktopAd" src="/ads/toyotaAD.jpeg"" alt="Vertical Ad" ></div>
-	<div id="verticalAdSlot_Mobile"><img class="mobileAd" src="/ads/appleWatchAD.png"" alt="Mobile Vertical Ad" ></div>
+	<div id="verticalAdSlot"><img class="desktopAd" src="/images/old-state-house.jpg"" alt="Massachusetts Old State House" ></div>
+	<div id="verticalAdSlot_Mobile"><img class="mobileAd" src="/images/charles-river-skyline-mobile.jpg"" alt="Boston Skyline from Charles River" ></div>
 </div>
 
