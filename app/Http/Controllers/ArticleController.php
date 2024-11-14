781088ea86a8fc798997c29c63ac32da3b2f7e13
photocurio <sunraydust@gmail.com>
2024-11-14 16:56:52 -0500

    filter the content to change boston.com to archive.boston.com

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -17,10 +17,23 @@ class ArticleController extends Controller
             abort(404);
         }
 
+        $filteredContent = $this->filterContent($article['content']);
+
         return View::make('article', [
             'title' => $article['title'],
-            'content' => $article['content'],
+            'content' => $filteredContent,
             'description' => $article['description']
         ])->render();
+    }
+
+    /**
+     * Filter the content, to change links to boston.com to archive.boston.com.
+     */
+    protected function filterContent($content)
+    {
+        // Use regex to match //boston.com or //www.boston.com
+        $pattern = '/\/\/(www\.)?boston\.com/';
+        $replacement = '//archive.boston.com';
+        return preg_replace($pattern, $replacement, $content);
     }
 }
