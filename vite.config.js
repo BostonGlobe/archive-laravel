6c687ebaf1c1566c14277939c0d1ea57d45f4cbe
photocurio <sunraydust@gmail.com>
2024-11-15 16:33:42 -0500

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ export default defineConfig({
     plugins: [
         laravel(['resources/css/app.scss']),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
